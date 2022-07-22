98788ce3500e103dbe34c4659def3b45444c7354
Artem Borodin <a.borodin2@g.nsu.ru>
2022-07-22 16:50:11 +0700

    Postgres with mm archive for termux package

--- cross-compile/aarch64.sh
+++ cross-compile/aarch64.sh
@@ -1,29 +1,24 @@
 #!/bin/bash
 
-sudo apt install gcc-aarch64-linux-gnu binutils-aarch64-linux-gnu # for armv8/aarch64
-sudo apt install gcc-arm-linux-gnueabi binutils-arm-linux-gnueabi # for armv7/aarch32
+# Install Android NDK
+curl https://dl.google.com/android/repository/android-ndk-r25-linux.zip -O
+unzip android-ndk-r25-linux.zip
+echo "export PATH=$PATH:$HOME/android-ndk-r25/toolchains/llvm/prebuilt/linux-x86_64/bin" >> ~/.bashrc
+source ~/.bashrc
 
+# Install compiler for aarch64 (not needed)
+sudo apt install gcc-aarch64-linux-gnu binutils-aarch64-linux-gnu # for armv8/aarch64
 
 # compile postgres for aarch64 (not working yet)
 mkdir -p aarch64-postgres
 cd aarch64-postgres
-../postgres_cluster/configure --host=aarch64-linux-gnu --without-readline --without-zlib --prefix=$PWD/packaged
+../postgres_cluster/configure --host=aarch64-linux-android28 --without-readline --without-zlib --prefix=$PWD/postgres-aarch64 CC=aarch64-linux-android28-clang USE_DEV_URANDOM=1
 make
 make install
+tar -czf postgres-aarch64.tar.gz postgres-aarch64/ --hard-dereferenc
 cd ../
 
 # Errors:
-# /postgres_cluster/src/port/pg_crc32c_armv8_choose.c:88:20: error: ‘pg_comp_crc32c_armv8’ undeclared (first use in this function); did you mean ‘pg_comp_crc32c_sb8’?
 
 
 
-# compile postgres for arm 32 bit (not working)
-
-mkdir -p arm-postgres
-cd arm-postgres
-../postgres_cluster/configure CC=arm-linux-gnueabi-gcc CPP=arm-linux-gnueabi-cpp USE_DEV_URANDOM=1 --host=arm-linux --without-readline --without-zlib --disable-spinlocks
-make
-cd ../
-
-# Errors:
-# /postgres_cluster/src/include/pg_config.h:772:24: error: ‘__int128’ is not supported on this target
